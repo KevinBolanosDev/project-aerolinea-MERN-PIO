4ee923834a6430a07f7e43a58e87749202fb3bb8
KevinBolanosDev <im.kevinbolanos.dev@gmail.com>
2025-02-09 13:48:32 -0500

    add environment variable

--- src/database/db.js
+++ src/database/db.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    // await mongoose.connect(process.env.MONGO_URI);
-    await mongoose.connect(process.env.MONGO_URI_LOCAL);
+    await mongoose.connect(process.env.MONGO_URI);
+    // await mongoose.connect(process.env.MONGO_URI_LOCAL);
     console.log(">>> DB is connected");
   } catch (error) {
     console.log("Something went wrong!");
